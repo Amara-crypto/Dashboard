f0b843294bcf7a65d20270134c64048f22c4d951
fredybangs <alfredbangura77@gmail.com>
2020-06-06 14:20:03 +0000

    Aded changes

--- src/App.js
+++ src/App.js
@@ -15,53 +15,88 @@ const { Title } = Typography;
 
 
 
-function App(){
-    return(
-           <div className="container">
-                <Layout>
-                <Header style={{padding:10 }} >
-                    
-                <Title style={{color:"white"}} level={4}>Blue Bird</Title>
-                <Avatar style={{float:"right",paddingBottom:10}} size="large"src='./iconfinder_7_avatar_2754582.png' />
-                </Header>
-                    <Layout>
-                    <Sider >
-                      <Menu
-                      defaultSelectedKeys={["Dashboard"]}
-                      mode="inline"
-                      >
-                          <MenuItem key="Dashboard">Dashboard</MenuItem>
-                          <SubMenu
-                          title={
-                           <span>
-                              <MailOutlined />
-                              <span>About Us</span>
-                           </span>
-                          }   
-                          >               
-                                    <Menu.ItemGroup key="Aboutus" title="country ">
-                                  <Menu.Item key="location1">Location</Menu.Item>
-                                  <Menu.Item key="location2">Location 2</Menu.Item> 
-                              </Menu.ItemGroup>
-                          </SubMenu>
-                      </Menu>
-                    </Sider>
-                    <Layout> 
-                        <Content style={{padding:'0 50px'}}>
-                           <Breadcrumb style={{margin:'16px 0'}}>
-                           <Breadcrumb.Item>Dashboard</Breadcrumb.Item>
-                            </Breadcrumb> 
-                            <div style={{background: '#FFF', padding:24, minHeight: 430,}}> Content</div>
-                        
-                        </Content>
-                        
-                        <Footer style={{ textAlign: 'center' }}>Byte Design ©2020 Created by Thomas UED</Footer>
-                     </Layout>  
-                    </Layout>
-                </Layout>
-            </div>
-        
-    );
+class  App extends React.Component(){
+    constructor(props){
+        this.state={
+            dashboard:true,
+            location1: false,
+            location2:false,
+        }
+    }
+    render(){
+        var dashboard= true;
+        var location1= false;
+        var location2= false;
+        return(
+            <div className="container">
+                 <Layout>
+                 <Header style={{padding:10 }} >
+                     
+                 <Title style={{color:"white"}} level={4}>Blue Bird</Title>
+                 <Avatar style={{float:"right",paddingBottom:10}} size="large"src='./iconfinder_7_avatar_2754582.png' />
+                 </Header>
+                     <Layout>
+                     <Sider >
+                       <Menu
+                       defaultSelectedKeys={["Dashboard"]}
+                       mode="inline"
+                       >
+                           <MenuItem key="Dashboard">Dashboard</MenuItem>
+                           <SubMenu
+                           title={
+                            <span>
+                               <MailOutlined />
+                               <span>About Us</span>
+                            </span>
+                           }   
+                           >               
+                                     <Menu.ItemGroup key="Aboutus" title="country ">
+                                   <Menu.Item key="location1" onClick={()=>{
+                                       location1=true;
+                                       dashboard=false;
+                                       location2=false
+                                   }}>Location</Menu.Item>
+                                   <Menu.Item key="location2">Location 2</Menu.Item> 
+                               </Menu.ItemGroup>
+                           </SubMenu>
+                       </Menu>
+                     </Sider>
+                     <Layout> 
+                         {dashboard===true?
+                         <Content style={{padding:'0 50px'}}>
+                         <Breadcrumb style={{margin:'16px 0'}}>
+                         <Breadcrumb.Item>Dashboard</Breadcrumb.Item>
+                          </Breadcrumb> 
+                          <div style={{background: '#FFF', padding:24, minHeight: 430,}}> Content</div>
+                      
+                      </Content>:null}
+                         
+                      {location1===true?
+                         <Content style={{padding:'0 50px'}}>
+                         <Breadcrumb style={{margin:'16px 0'}}>
+                         <Breadcrumb.Item>Dashboard</Breadcrumb.Item>
+                          </Breadcrumb> 
+                          <div style={{background: '#FFF', padding:24, minHeight: 430,}}> Location 1</div>
+                      
+                      </Content>:null}
+                      {location2===true?
+                         <Content style={{padding:'0 50px'}}>
+                         <Breadcrumb style={{margin:'16px 0'}}>
+                         <Breadcrumb.Item>Dashboard</Breadcrumb.Item>
+                          </Breadcrumb> 
+                          <div style={{background: '#FFF', padding:24, minHeight: 430,}}> Location2</div>
+                      
+                      </Content>:null}
+                         <Footer style={{ textAlign: 'center' }}>Byte Design ©2020 Created by Thomas UED</Footer>
+                      </Layout>  
+                     </Layout>
+                 </Layout>
+             </div>
+         
+     );
+
+    }
+    
 }
 export default App;
 
