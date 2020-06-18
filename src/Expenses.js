a3f89c3faee122c6a680cbe6c10210cf07a3d4fa
Amara-crypto <thomastarawali@gmail.com>
2020-06-18 15:03:56 +0000

    ADED FOO

--- src/Expenses.js
+++ src/Expenses.js
@@ -1,12 +1,42 @@
 import React from 'react';
+import Component from 'react';
 import { Row, Col } from 'antd';
+import {Image} from  'react-bootstrap';
 
-const Expenses = () => (
-  <Row>
-  <Col span={6}> <h2>Task</h2>
-    <p>State at ceiling lay on arms while you're using the keyboard so this human feeds me.</p>
-    <p>I am a kitty cat, sup, feed me, no cares in the world</p>
-    <p>Meow meow, I tell my human purr for no reason but to chase after</p></Col>
-</Row>
-);
+class Expenses extends Component(){
+
+  render(){
+    return(
+      <view><Row>
+
+      </Row>
+
+      </view>
+    )
+  }
+
+  renderItms(){
+    var  lis = []
+    
+     var items = [{name: 'Samsung Note 4', price: 4000, image:''},
+     {name: 'Samsung Note 4', price: 4000, image:''},
+     {name: 'Samsung Note 4', price: 4000, image:''},{
+       name: 'Samsung Note 4', price: 4000, image:''}]
+       for(var x=0; x>items.length; x++){
+        lis.push(
+                <Col xs={{ span: 5, offset: 1 }} lg={{ span: 6, offset: 2 }}>
+            <view  style={{ paddingBottom:0, margin:0}}>
+            <Image width='240' height='240' src={require('./images/s10.jpg')}/>
+            <span style={{color:'blue', fontweight:'bold', alignItems:'center' }}>Samsung S10</span>
+            <p>$500</p>
+            </view>
+            
+          </Col>
+        )
+    
+      }
+   }
+
+
+}
 export default Expenses
